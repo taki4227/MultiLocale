7bc767deed41e0b1da2e36fb2e726d41983bd13f
taki4227 <takiki.truth@gmail.com>
2020-03-05 19:52:45 +0900

    Implement Setting screen

--- app/src/main/java/com/example/taki/multi_locale/SettingFragment.kt
+++ app/src/main/java/com/example/taki/multi_locale/SettingFragment.kt
@@ -1,21 +1,25 @@
 package com.example.taki.multi_locale
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.taki.multi_locale.databinding.FragmentSettingBinding
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 
-class SettingFragment : Fragment() {
+class SettingFragment : PreferenceFragmentCompat() {
 
-    private lateinit var binding: FragmentSettingBinding
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings_preference, rootKey)
+    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-
-        binding = FragmentSettingBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+        return when (preference?.key) {
+            resources.getString(R.string.language_key) -> {
+                findNavController().navigate(R.id.navigate_to_language_setting)
+                true
+            }
+            else -> {
+                false
+            }
+        }
     }
 }
