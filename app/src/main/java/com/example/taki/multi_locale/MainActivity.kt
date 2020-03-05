8ca9ed50ed13eb96e7a30f0cbfb3c3de3fc9c4b3
taki4227 <takiki.truth@gmail.com>
2020-03-05 19:02:33 +0900

    Use navigation component

--- app/src/main/java/com/example/taki/multi_locale/MainActivity.kt
+++ app/src/main/java/com/example/taki/multi_locale/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.taki.multi_locale
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.taki.multi_locale.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
